045d8d7ee4c824344d1e93ddbd647ea4ce7bde11
amin <aminqanbarii1992@gmail.com>
2022-02-26 06:24:26 -0800

    complete todoForm and todoList(create function) and ...

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,33 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function TodoForm(props) {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
 
-  const handleChange = e => {
-      setInput(e.target.value)
-  }
+  const inputRef = useRef(null);
 
-  const handleSubmit = e => {
-      e.preventDefault()
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
-      props.onSubmit({
-          id: Math.floor(Math.random() * 10000) ,
-          text : input
-      })
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
-      setInput('')
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+
+    setInput("");
+  };
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Add a todo"
-        value={input}
-        name="text"
-        className="todo-input"
-        onChange={handleChange}
-      />
-      <button className="todo-button">Add todo</button>
+      {props.edit ? (
+        <>
+          {" "}
+          <input
+            type="text"
+            placeholder="Update your item"
+            value={input}
+            name="text"
+            className="todo-input edit"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button edit">Update</button>
+        </>
+      ) : (
+        <>
+          {" "}
+          <input
+            type="text"
+            placeholder="Add a todo"
+            value={input}
+            name="text"
+            className="todo-input"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button">Add todo</button>
+        </>
+      )}
     </form>
   );
 }
